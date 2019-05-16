357e3be01ab4d630774473a193e90b77cee61066
SudarshanKumar143341 <48338961+SudarshanKumar143341@users.noreply.github.com>
2019-05-17 04:48:12 +0930

    Update minimum.js

--- js/minimum.js
+++ js/minimum.js
@@ -5,3 +5,8 @@ function toggleSidebar(){
 
 
 
+/*Function to navigate back to top*/
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
